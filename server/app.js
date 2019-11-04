cddb4b5ff7335e11f825a3033f059128c5d7a679
Nicole <nscampbell303@gmail.com>
2019-11-04 18:04:47 -0500

    Modified for mongooseDB

--- server/app.js
+++ server/app.js
@@ -1,22 +1,30 @@
-import createError from "http-errors";
-import express, { json, urlencoded } from "express";
-import { join } from "path";
-import cookieParser from "cookie-parser";
-import logger from "morgan";
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
 
-import indexRouter from "./routes/index";
-import pingRouter from "./routes/ping";
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(logger("dev"));
-app.use(json());
-app.use(urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(join(__dirname, "public")));
+mongoose.connect("mongodb://localhost:27017/babyregistry", { useNewUrlParser: true }, err => 
+  err ? console.log('Error: ', err) : console.log('MongoDB is connected!'));
 
-app.use("/", indexRouter);
-app.use("/ping", pingRouter);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -27,11 +35,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.json({ error: err });
+  res.render('error');
 });
 
 module.exports = app;
