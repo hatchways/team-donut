276eadbf95bc64a6a612820076771ec05f718804
ManpreetBhuller <40129775+ManpreetBhuller@users.noreply.github.com>
2019-11-09 16:14:47 -0500

    Formatted
    Formatted nicely

--- client/src/LogIn.js
+++ client/src/LogIn.js
@@ -5,31 +5,31 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 export default class LogIn extends Component {
-constructor(){
-  super();
-  this.state={
-    email:'',
-    password:''
-  };
-}
-
-handleChange = event =>{
-  this.setState({[event.target.name]: event.target.value});
-}
-
-handleSubmit = event =>{
-  event.preventDefault();
-
-  const userData = {
-      email:this.state.email,
-      password:this.state.password
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: ''
+    };
   }
 
-  axios.post('http://localhost:4000/api/user/login', userData)
-  .then(res=>{
-    console.log(res);
-  });
-}
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const userData = {
+      email: this.state.email,
+      password: this.state.password
+    }
+
+    axios.post('http://localhost:4000/api/user/login', userData)
+      .then(res => {
+        console.log(res);
+      });
+  }
   signup = () => {
     window.location.href = "/signup"
   }
@@ -40,34 +40,34 @@ handleSubmit = event =>{
 
   render() {
     return (
-      <div style={{display: 'flex'}}>
+      <div style={{ display: 'flex' }}>
 
-      <div className="col-lg-6">
-        <img src={babyFund} className="logo" />
+        <div className="col-lg-6">
+          <img src={babyFund} className="logo" />
 
-        <form id="login" onSubmit={this.handleSubmit}>
-          <h3 className="mb-5">Log In</h3>
+          <form id="login" onSubmit={this.handleSubmit}>
+            <h3 className="mb-5">Log In</h3>
 
-          <div className="form-group">
-            <label>Email address</label>
-            <input type="email" className="form-control" name='email' onChange={this.handleChange} value={this.state.email} placeholder="Enter email" />
-          </div>
+            <div className="form-group">
+              <label>Email address</label>
+              <input type="email" className="form-control" name='email' onChange={this.handleChange} value={this.state.email} placeholder="Enter email" />
+            </div>
 
-          <div className="form-group">
-            <label>Password</label>
-            <input type="password" className="form-control" name='password' onChange={this.handleChange} value={this.state.password} placeholder="Password" />
-          </div>
+            <div className="form-group">
+              <label>Password</label>
+              <input type="password" className="form-control" name='password' onChange={this.handleChange} value={this.state.password} placeholder="Password" />
+            </div>
 
-          <Link to="/forgotpswd" style={{color: 'black'}}>Forgot Password?</Link>
-          <br />
+            <Link to="/forgotpswd" style={{ color: 'black' }}>Forgot Password?</Link>
+            <br />
 
-          <button type="submit" className="btn btn-dark login" >Log In</button>    
-        </form>
-      </div>
+            <button type="submit" className="btn btn-dark login" >Log In</button>
+          </form>
+        </div>
 
-      <div className="col-lg-6 bigPic">
-        <button type="button" className="btn btn-outline-light signup" onClick={this.signup}>Sign Up</button>
-      </div>
+        <div className="col-lg-6 bigPic">
+          <button type="button" className="btn btn-outline-light signup" onClick={this.signup}>Sign Up</button>
+        </div>
 
       </div>
     )
