d511986a823138af19e1390a240491e6fedbbae2
ManpreetBhuller <40129775+ManpreetBhuller@users.noreply.github.com>
2019-11-09 16:12:44 -0500

    updated-Signup&Login
    Sign-Up/Login integrated with front end.
    When user do login or sign-up then we get data in response to the browser.

--- client/src/LogIn.js
+++ client/src/LogIn.js
@@ -2,16 +2,41 @@ import React, { Component } from 'react'
 import './App.css';
 import babyFund from './babyFund.png';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default class LogIn extends Component {
+constructor(){
+  super();
+  this.state={
+    email:'',
+    password:''
+  };
+}
 
+handleChange = event =>{
+  this.setState({[event.target.name]: event.target.value});
+}
+
+handleSubmit = event =>{
+  event.preventDefault();
+
+  const userData = {
+      email:this.state.email,
+      password:this.state.password
+  }
+
+  axios.post('http://localhost:4000/api/user/login', userData)
+  .then(res=>{
+    console.log(res);
+  });
+}
   signup = () => {
     window.location.href = "/signup"
   }
 
-  loggedin = () => {
-    console.log('logged in!')
-  }
+  // loggedin = () => {
+  //   console.log('logged in!')
+  // }
 
   render() {
     return (
@@ -20,23 +45,23 @@ export default class LogIn extends Component {
       <div className="col-lg-6">
         <img src={babyFund} className="logo" />
 
-        <form id="login">
+        <form id="login" onSubmit={this.handleSubmit}>
           <h3 className="mb-5">Log In</h3>
 
           <div className="form-group">
             <label>Email address</label>
-            <input type="email" className="form-control" placeholder="Enter email" />
+            <input type="email" className="form-control" name='email' onChange={this.handleChange} value={this.state.email} placeholder="Enter email" />
           </div>
 
           <div className="form-group">
             <label>Password</label>
-            <input type="password" className="form-control" placeholder="Password" />
+            <input type="password" className="form-control" name='password' onChange={this.handleChange} value={this.state.password} placeholder="Password" />
           </div>
 
           <Link to="/forgotpswd" style={{color: 'black'}}>Forgot Password?</Link>
           <br />
 
-          <button type="button" className="btn btn-dark login" onClick={this.loggedin}>Log In</button>    
+          <button type="submit" className="btn btn-dark login" >Log In</button>    
         </form>
       </div>
 
