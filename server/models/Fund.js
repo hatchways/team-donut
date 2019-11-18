1284f2d44c158b95c4968365fb5d1ef4ac09525a
ManpreetBhuller <40129775+ManpreetBhuller@users.noreply.github.com>
2019-11-17 22:21:54 -0500

    Edited fund model
    photo entity added.

---
+++ server/models/Fund.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const FundSchema = mongoose.Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'user'
+    },
+    name:{
+        type: String,
+        required:true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    goal:{
+        type: String,
+        required: true
+    },
+    deadline:{
+        type: Date,
+        default: Date.now
+    },
+    photo: {
+        type: [{
+            url:{
+                type:String
+            }
+        }]
+    }
+});
+
+const Fund = mongoose.model('fund', FundSchema);
+
+module.exports = Fund;
+
