9c419620364a00de27ce12016dd9502b575b421b
ManpreetBhuller <40129775+ManpreetBhuller@users.noreply.github.com>
2019-11-18 00:24:20 -0500

    Nothing changed
    only styling of fund model

--- server/models/Fund.js
+++ server/models/Fund.js
@@ -1,28 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const FundSchema = mongoose.Schema({
-    user:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref:'user'
-    },
-    name:{
-        type: String,
-        required:true
-    },
-    description:{
-        type: String,
-        required: true
-    },
-    goal:{
-        type: String,
-        required: true
-    },
-    deadline:{
-        type: Date,
-        default: Date.now
-    }
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user"
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  goal: {
+    type: String,
+    required: true
+  },
+  deadline: {
+    type: Date,
+    default: Date.now
+  },
+  photo: {
+    type: [String]
+  }
 });
 
-const Fund = mongoose.model('fund', FundSchema);
+const Fund = mongoose.model("fund", FundSchema);
 
 module.exports = Fund;
